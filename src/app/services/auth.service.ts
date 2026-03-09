1d794dd7b97d64e096d0afa3597f9e5cc2ec4a67
Christopher <josephchristor4@gmail.com>
2026-03-09 18:08:41 +0530

    login page style

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-
 @Injectable({
   providedIn: 'root'
 })
@@ -9,26 +8,28 @@ export class AuthService {
 
   private apiUrl = 'http://localhost:3000';
 
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   login(username: string, password: string) {
-    return this.http.post(`${this.apiUrl}/login`, {
+    return this.http.post<{ token: string }>(`${this.apiUrl}/login`, {
       username,
       password
-    })
+    });
   }
 
   saveToken(token: string) {
-    localStorage.setItem('authToken', token)
+    localStorage.setItem('authToken', token);
+  }
 
+  getToken(): string | null {
+    return localStorage.getItem('authToken');
   }
 
   logout() {
-    localStorage.removeItem('authToken')
+    localStorage.removeItem('authToken');
   }
 
-  isloggedIn(): boolean {
-    return !!localStorage.getItem('authToken')
+  isLoggedIn(): boolean {
+    return !!localStorage.getItem('authToken');
   }
 }
